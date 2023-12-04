4c47e4991062fc8ca802c80597656f70f13208f4
Anton <An_freiheit@mail.ru>
2023-12-04 18:55:13 +0300

    first commit

---
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -0,0 +1,24 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static sun.jvm.hotspot.interpreter.Interpreter.codeField;
+
+public class VerificationPage {
+    private final SelenideElement codeFinal = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}
